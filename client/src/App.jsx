5a06b7a059b793717ec00721074cf7cf9c44462f
waleed khan <waleedkhansafimd983@gmail.com>
2025-10-04 22:09:36 -0700

    install react js and tailwind css and create first template

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+ import React from 'react'
+ 
+ const App = () => {
+   return (
+     <div>
+       <h1 className='text-blue-700 text-3xl font-bold '>Mern Blog</h1>
+       <p className='text-gray-700'>Welcome to the Mern Blog!</p>
+
+     </div>
+   )
+ }
+ 
+ export default App
+ 
